2d2078769fe9e5b24f7734337957474910793470
Fletcher Fowler <fletch@fzf.me>
2024-07-14 08:27:52 -0400

    Adding comment for example

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,7 @@ config.resolver.unstable_enablePackageExports = true
 config.resolver.extraNodeModules = {
   crypto: require.resolve('expo-crypto'),
 };
+// https://github.com/facebook/metro/issues/1272
 config.resolver.unstable_conditionNames = [
   'browser',
   'require',
